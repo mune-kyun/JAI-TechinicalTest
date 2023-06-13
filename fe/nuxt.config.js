4aac56f36850bcfb70e504002abc85144990a3c3
mune-kyun <rzknurfaizi@gmail.com>
2023-06-13 16:10:56 +0700

    fe: meta

--- fe/nuxt.config.js
+++ fe/nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'fe',
+    title: 'Orudoro',
     htmlAttrs: {
       lang: 'en',
     },
@@ -16,7 +16,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/letter-o.png' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
