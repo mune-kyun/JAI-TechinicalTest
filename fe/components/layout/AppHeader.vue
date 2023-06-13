15fe8a058305e2f3d24454db1c76d450c8e8cec9
mune-kyun <rzknurfaizi@gmail.com>
2023-06-13 16:04:15 +0700

    fe: index page

--- fe/components/layout/AppHeader.vue
+++ fe/components/layout/AppHeader.vue
@@ -4,11 +4,7 @@
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
       <nuxt-link to="/" class="flex items-center">
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          class="h-8 mr-3"
-          alt="Flowbite Logo"
-        />
+        <img src="/letter-o.png" class="h-8 mr-3" alt="logo" />
         <span
           class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
           >Orudoro</span
--- fe/pages/index.vue
+++ fe/pages/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="w-full">
-    <h1>Hello {{ username }}</h1>
+  <div class="h-full w-full flex py-10 pt-24 px-10 justify-center">
+    <div class="flex flex-col items-center justify-center gap-6">
+      <h1 class="animate-[fade-in_1s_ease-in-out] text-5xl">
+        Hello <span class="text-cyan-700">{{ username }}</span>
+      </h1>
+      <nuxt-link
+        to="/order"
+        class="rounded-full py-4 px-10 bg-white text-cyan-500 border border-cyan-500 text-xl font-medium hover:bg-cyan-600 hover:text-white"
+        >See All Order</nuxt-link
+      >
+    </div>
   </div>
 </template>
 
